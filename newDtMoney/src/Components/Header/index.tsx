ae84a77a7911c1a432d0b8b4c2c66e20c370e340
Vinicius Benso <vinibenso@gmail.com>
2022-08-10 16:52:32 -0300

    create modal

--- newDtMoney/src/Components/Header/index.tsx
+++ newDtMoney/src/Components/Header/index.tsx
@@ -1,5 +1,7 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImg from '../../assets/logo.svg'
+import * as Dialog from '@radix-ui/react-dialog';
+import { NewTransactionModal } from "../NewTransactionModal";
 
 
 
@@ -8,15 +10,20 @@ export function Header() {
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <NewTransactionButton>
-          Nova Transação
-        </NewTransactionButton>
 
 
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>
+              Nova Transação
+            </NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal/>
+
+        </Dialog.Root>
 
       </HeaderContent>
-
-  
     </HeaderContainer>
   )
 }
